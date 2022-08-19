500691a63f81f8c560fbba0437885692fe6babe3
AntonioCaue <acompcelular@gmail.com>
2022-08-19 12:57:02 -0300

    .gitignore mais completo

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
+//Faz o fatorial recursivamente
 int fatorial(int n){
-    if(n == 0){
+    if(n == 0){//"Enquanto n != 0 "
         return 1;
     }else{
-        return n * fatorial(n-1);
+        return n * fatorial(n-1);//recursivo
     }
 }
 
+//Funcao principal
 int main(){
     printf("Hello Git\n");
-    printf("Fatorial(5) = %d\n", fatorial (5));
+    printf("Fatorial(5) = %d\n", fatorial (5));//fatorial de n = 5
 }
